221d30c6c643d121ecf85317f835181170f2d9cf
Erik Solen <esolen@yieldbot.com>
2015-11-19 22:49:07 -0500

    refactor: drop dead code and don't use JS for active main nav

--- js/scripts.js
+++ js/scripts.js
@@ -16,29 +16,6 @@ if ( $(window).width() <= 1024 ) {
 		}
 	);
 }
-var page_name = location.pathname;
-page_name = page_name.toLowerCase();
-page_name = page_name.replace('/','');
-$('#main-nav').find('li').each(
-	function() {
-		var mn_page_name = $(this).find('a').attr('href');
-		mn_page_name = mn_page_name.toLowerCase();
-		mn_page_name = mn_page_name.replace('/','');
-		if (page_name == mn_page_name) {
-			$(this).find('a').addClass('mn-link-active');
-		}
-	}
-);
 // end main navigation
-
-// begin hiding blue content section under main nav when it is an empty div
-$('.top-w-3').each(
-	function() {
-		if ( ( $(this).html() == '' ) || ( $(this).html() == '<!-- optional text can be placed here -->' ) ) {
-			$(this).css({'display':'none'});
-		}
-	}
-);
-// end hiding blue content section under main nav when it is an empty div
 
 }); // end document ready

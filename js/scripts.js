f9eeaa3886454b81663737fa18634af61332484b
John W. Ellis <jellis@yieldbot.com>
2016-10-10 15:37:10 -0400

    Revert eol termination "Pubfood Site w Analytics"
    This reverts commit d96cc065165d958bef2e306174855cea80bfbc8f.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,22 @@
 jQuery(document).ready(function($) {
 
-var the_window_width = $(window).width();
-
 // begin main navigation
 $('#mn-toggle-link').click(
 	function() {
 		$('#main-nav').stop().slideToggle(500);
 	}
 );
-$(window).resize(
-	function() {
-		if ( $(this).width() != the_window_width && $(this).width() >= 1000 ) {
-			$('#main-nav').stop().css({'display':''});
+if ( $(window).width() <= 1024 ) {
+	var the_mn_window_width = $(window).width();
+	$(window).resize(
+		function() {
+			if ( $(this).width() != the_mn_window_width ) {
+				$('#main-nav').stop().css({'display':''});
+			}
 		}
-	}
-);
+	);
+}
 // end main navigation
-
-// begin "Example GA Reports" show/hide sections
-$('#example-ga-reports-menu').find('li').click(
-	function() {
-		var section_id_to_display = $(this).find('a').attr('href');
-		$('#adunit, #device, #latency').hide(0);
-		$(section_id_to_display).fadeIn(500);	
-	}
-);
-// end  "Example GA Reports" show/hide sections
 
   //Set nav content anchors in generated content
   if (window.apiContentAnchors) {
